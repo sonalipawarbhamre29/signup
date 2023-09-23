88c2d09f79886017aa7f37c4a4aea77649feae48
sonalipawar <124123724+sonalipawarbhamre29@users.noreply.github.com>
2023-09-23 17:42:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function getFormvalue() {
+    //Write your code here
+	function getFormvalue() {
+    //Write your code here
+	let firstName = document.querySelector("#firstName").value;
+	let lastName = document.querySelector("#lastName").value;
+	alert(firstName + " " + lastName);
+}
+
+}
